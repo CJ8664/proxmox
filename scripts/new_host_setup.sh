ae462c5b9df1bcdcbc8793609c4502a91c81c303
Chirag Jain <er.chiragjain92@gmail.com>
2024-07-05 04:58:37 +0000

    ohmyzsh install

--- scripts/new_host_setup.sh
+++
@@ -1,9 +0,0 @@
-# Install the essentials
-apt install git vim curl sudo dpkg wget zsh -y
-
-# Schedule automatic updates
-apt install unattended-upgrades -y
-dpkg-reconfigure --priority=low --frontend noninteractive unattended-upgrades
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <<< y
